14a199e5cb498233177c440ec7782fc2512b2ae3
maletinchess <maletinchess2017@gmail.com>
2021-05-20 20:47:36 +0700

    add dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,28 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          'style-loader', // inject CSS to page
+          'css-loader', // translates CSS into CommonJS modules
+          'postcss-loader',
+          'sass-loader', // compiles Sass to CSS
+        ],
       },
     ],
   },
