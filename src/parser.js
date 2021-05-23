b6ec64df1553435a69cc6339640933a83ca1ff92
maletinchess <maletinchess2017@gmail.com>
2021-05-23 20:55:58 +0700

    fix console posts

--- src/parser.js
+++ src/parser.js
@@ -14,7 +14,7 @@ const parse = (xml) => {
   const description = docXml.querySelector('description').textContent;
 
   const feed = { title, description };
-  const items = document.querySelectorAll('item');
+  const items = docXml.querySelectorAll('item');
   const posts = [...items];
   console.log(posts);
   return { feed };
